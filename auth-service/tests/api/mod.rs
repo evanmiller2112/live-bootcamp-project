eddc062a8338ff5713644f34dbadf849e4706fb9
Evan Miller <evanmiller2112@gmail.com>
2024-10-11 13:41:21 -0400

    Sprint 1 Task 8 Completed

--- auth-service/tests/api/mod.rs
+++
@@ -1,7 +0,0 @@
-mod helpers;
-mod login;
-mod logout;
-mod root;
-mod signup;
-mod verify_2fa;
-mod verify_token;
--- auth-service/tests/api/signup.rs
+++ auth-service/tests/api/signup.rs
@@ -1,7 +1,26 @@
-use crate::helpers::TestApp;
+use auth_service::routes::signup;
+use crate::helpers;
+use crate::helpers::{get_random_email, TestApp};
+
 #[tokio::test]
-pub async fn signup_returns_200() {
-    let app = TestApp::new().await;
-    let response = app.signup().await;
-    assert_eq!(response.status().as_u16(), 200);
+pub async fn should_return_422_if_malformed_input() {
+    let app: TestApp = TestApp::new().await;
+    let random_email: String = helpers::get_random_email();
+
+    let test_cases = [
+        serde_json::json!({
+            "password": "password123",
+            "requires2FA": true
+        }),
+    ];
+
+    for test_case in test_cases.iter() {
+        let response = app.post_signup(&test_cases);
+        assert_eq!(
+            response.await.status().as_u16(),
+            422,
+            "Failed for input: {:?}",
+            test_case
+        );
+    }
 }
