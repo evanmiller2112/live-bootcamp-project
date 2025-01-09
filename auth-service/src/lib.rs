8440c5771f1de913432a9a9429245a1328e9b3f3
Evan Miller <evanmiller2112@gmail.com>
2025-01-09 15:01:10 -0500

    [Sprint 1][Task 4]

---
+++ auth-service/src/lib.rs
@@ -0,0 +1,33 @@
+// This struct encapsulates our application-related logic.
+use axum::Router;
+use axum::serve::Serve;
+use std::error::Error;
+
+pub struct Application {
+    server: Serve<Router, Router>,
+    pub address: String,
+}
+
+impl Application {
+    pub async fn build(address: &str) -> Result<Self, Box<dyn Error>> {
+        // Move the Router definition from `main.rs` to here.
+        // Also, remove the `hello` route.
+        // We don't need it at this point!
+        let router = axum::Router::new();
+
+        let listener = tokio::net::TcpListener::bind(address).await?;
+        let address = listener.local_addr()?.to_string();
+        let server = axum::serve(listener, router);
+
+        let app: Application = Self {
+            server,
+            address,
+        };
+        Result::Ok(app)
+    }
+
+    pub async fn run(self) -> Result<(), std::io::Error> {
+        println!("listening on {}", &self.address);
+        self.server.await
+    }
+}
