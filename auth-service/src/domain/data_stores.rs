646bdc8586b0ce6e5e7b10d8ebdbe291728c34ab
Evan Miller <evanmiller2112@gmail.com>
2025-02-05 15:13:47 -0500

    [Sprint 4][Task 3][Step 7] Complete

--- auth-service/src/domain/data_stores.rs
+++ auth-service/src/domain/data_stores.rs
@@ -1,4 +1,5 @@
 use rand::Rng;
+
 use super::{Email, Password, User};
 
 #[async_trait::async_trait]
@@ -9,13 +10,25 @@ pub trait UserStore {
                            -> Result<(), UserStoreError>;
 }
 
+#[derive(Debug, PartialEq)]
+pub enum UserStoreError {
+    UserAlreadyExists,
+    UserNotFound,
+    InvalidCredentials,
+    UnexpectedError,
+}
+
 #[async_trait::async_trait]
 pub trait BannedTokenStore {
     async fn add_token(&mut self, token: String) -> Result<(), BannedTokenStoreError>;
     async fn contains_token(&self, token: &str) -> Result<bool, BannedTokenStoreError>;
 }
 
-// This trait represents the interface all concrete 2FA code stores should implement
+#[derive(Debug)]
+pub enum BannedTokenStoreError {
+    UnexpectedError,
+}
+
 #[async_trait::async_trait]
 pub trait TwoFACodeStore {
     async fn add_code(
@@ -42,20 +55,15 @@ pub struct LoginAttemptId(String);
 
 impl LoginAttemptId {
     pub fn parse(id: String) -> Result<Self, String> {
-        // Use the `parse_str` function from the `uuid` crate to ensure `id` is a valid UUID
-        let result = match uuid::Uuid::parse_str(&id) {
-            Ok(uuid) => uuid,
-            Err(_) => return Err("Invalid UUID".to_string()),
-        };
-        let id = LoginAttemptId(result.to_string());
-        Ok(id)
+        let parsed_id =
+            uuid::Uuid::parse_str(&id).map_err(|_| "Invalid login attempt id".to_owned())?;
+        Ok(Self(parsed_id.to_string()))
     }
 }
 
 impl Default for LoginAttemptId {
     fn default() -> Self {
-        // Use the `uuid` crate to generate a random version 4 UUID
-        LoginAttemptId(uuid::Uuid::new_v4().to_string())
+        Self(uuid::Uuid::new_v4().to_string())
     }
 }
 
@@ -92,16 +100,4 @@ impl AsRef<str> for TwoFACode {
     fn as_ref(&self) -> &str {
         &self.0
     }
-}
-#[derive(Debug, PartialEq)]
-pub enum UserStoreError {
-    UserAlreadyExists,
-    UserNotFound,
-    InvalidCredentials,
-    UnexpectedError,
-}
-
-#[derive(Debug)]
-pub enum BannedTokenStoreError {
-    UnexpectedError,
 }
--- auth-service/src/routes/verify_2fa.rs
+++ auth-service/src/routes/verify_2fa.rs
@@ -1,55 +1,63 @@
-use serde::{Serialize, Deserialize};
-use axum::{extract::State, http::StatusCode, response::IntoResponse, Json};
+use axum::{extract::State, response::IntoResponse, Json};
+use axum_extra::extract::CookieJar;
+use serde::Deserialize;
+
 use crate::{
     app_state::AppState,
-    domain::{LoginAttemptId, TwoFACode, Email, AuthAPIError}
+    domain::{AuthAPIError, Email, LoginAttemptId, TwoFACode},
+    utils::auth::generate_auth_cookie,
 };
 
 pub async fn verify_2fa(
     State(state): State<AppState>,
-    Json(request): Json<Verify2FARequest>
-) -> Result<impl IntoResponse, AuthAPIError> {
-    let email = match Email::parse(request.email) {
+    jar: CookieJar,
+    Json(request): Json<Verify2FARequest>,
+) -> (CookieJar, Result<impl IntoResponse, AuthAPIError>) {
+    let email = match Email::parse(request.email.clone()) {
         Ok(email) => email,
-        Err(_) => return Err(AuthAPIError::InvalidCredentials),
+        Err(_) => return (jar, Err(AuthAPIError::InvalidCredentials)),
     };
-    
-    let login_attempt_id = match LoginAttemptId::parse(request.login_attempt_id) {
+
+    let login_attempt_id = match LoginAttemptId::parse(request.login_attempt_id.clone()) {
         Ok(login_attempt_id) => login_attempt_id,
-        Err(_) => return Err(AuthAPIError::InvalidCredentials),
+        Err(_) => return (jar, Err(AuthAPIError::InvalidCredentials)),
     };
-    
-    let two_fa_code = match TwoFACode::parse(request.two_factor_code) {
-        Ok(two_factor_code) => two_factor_code,
-        Err(_) => return Err(AuthAPIError::InvalidCredentials),
+
+    let two_fa_code = match TwoFACode::parse(request.two_fa_code) {
+        Ok(two_fa_code) => two_fa_code,
+        Err(_) => return (jar, Err(AuthAPIError::InvalidCredentials)),
     };
-    
+
     let mut two_fa_code_store = state.two_fa_code_store.write().await;
-    
+
     let code_tuple = match two_fa_code_store.get_code(&email).await {
         Ok(code_tuple) => code_tuple,
-        Err(_) => return Err(AuthAPIError::IncorrectCredentials),
+        Err(_) => return (jar, Err(AuthAPIError::IncorrectCredentials)),
     };
-    
+
     if !code_tuple.0.eq(&login_attempt_id) || !code_tuple.1.eq(&two_fa_code) {
-        return Err(AuthAPIError::InvalidCredentials);
+        return (jar, Err(AuthAPIError::IncorrectCredentials));
     }
-    
+
     if two_fa_code_store.remove_code(&email).await.is_err() {
-        return Err(AuthAPIError::UnexpectedError);
+        return (jar, Err(AuthAPIError::UnexpectedError));
     }
-    
-    Ok(StatusCode::OK.into_response())
-    
+
+    let cookie = match generate_auth_cookie(&email) {
+        Ok(cookie) => cookie,
+        Err(_) => return (jar, Err(AuthAPIError::UnexpectedError)),
+    };
+
+    let updated_jar = jar.add(cookie);
+
+    (updated_jar, Ok(()))
 }
 
-
-// If a user requires 2FA, this JSON body should be returned!
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Deserialize)]
 pub struct Verify2FARequest {
     pub email: String,
     #[serde(rename = "loginAttemptId")]
     pub login_attempt_id: String,
     #[serde(rename = "2FACode")]
-    pub two_factor_code: String,
+    pub two_fa_code: String,
 }
